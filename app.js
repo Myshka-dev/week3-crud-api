6f719b9b1da00ed46a9839bc48a8324dd25764e3
Myshka-dev <marie_myshka@hotmail.com>
2026-03-25 00:01:04 +0100

    feat: initialize express, build GET and POST routes API's

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.use(express.json()); // parse JSON bodies
+
+let todos = [
+    { id: 1, task: 'learn Node.js', completed: false},
+    { id: 2, task: 'build a CRUD API', completed: false},
+];
+
+// Get all todos
+app.get('/todos', (req, res) => {
+    res.status(200).json(todos); // send the list of todos as JSON
+});
+
+app.post('/todos', (req, res) => {
+    const newTodo = { id: todos.length + 1, task: req.body.task, completed: false }; 
+    todos.push(newTodo); // add the new todo to the list
+    res.status(201).json(newTodo); // send the created todo as JSON
+});
